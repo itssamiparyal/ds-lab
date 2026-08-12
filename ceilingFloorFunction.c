91db17b9577f4ba3a209d882375f28cb0979f8de
Samip Aryal <samiparyal25007@gmail.com>
2026-08-12 06:57:05 +0545

    celling Floor function

---
+++ ceilingFloorFunction.c
@@ -0,0 +1,17 @@
+//write a program to implement celling and floor function
+
+#include <stdio.h>
+#include <math.h>
+int main(){
+    float val;
+    float fVal,cVal;
+    printf("\n*****CEILING & FLOORING FUNCTIONS *****\n");
+    printf("Enter a float value: ");
+    scanf("%f",&val);
+    //floor function return the floor value of given argument
+    fVal = floor(val);
+    //ceil function return the floor value of given arguments
+    cVal = ceil(val);
+    printf("\nFloor value: %0.2f \nCeil value: %0.2f\n",fVal,cVal);
+    return 0;
+}
